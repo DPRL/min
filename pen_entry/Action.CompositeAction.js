732012e2c2a8d6d75883d32850e95f0ef41dc525
cms5347 <cms5347@ba091d2c-d542-4e25-8a58-bd8a2c7d4f9e>
2013-02-03 06:35:19 +0000

    Added a Composite action object for combining actions together. This was necessary for undoing momentum deletes
    git-svn-id: file:///mnt/raid/svn/mathfrontend@149 ba091d2c-d542-4e25-8a58-bd8a2c7d4f9e

---
+++ pen_entry/Action.CompositeAction.js
@@ -0,0 +1,47 @@
+/**
+   Composite Actions are comprised of several other actions.
+**/
+CompositeAction.prototype = new Action();
+
+function CompositeAction(){
+    this.action_list = new Array();
+}
+
+CompositeAction.prototype.shouldKeep = function(){
+    return true;
+}
+CompositeAction.prototype.toString = function(){
+    // cms: possibly extend to list constituents
+    return "CompositeAction";
+}
+
+/**
+   This function will add an action to the list of actions.
+**/
+CompositeAction.prototype.add_action = function(action){
+    console.log("adding action");
+    this.action_list.unshift(action);
+}
+
+/*
+  Running the Apply() method will execute the Apply methods of
+  the constituents in the order that they were added
+*/
+CompositeAction.prototype.Apply = function(){
+    for(var i = 0; i < this.action_list.length; i++){
+        this.action_list[i].Apply();
+    }
+}
+
+/*
+  Undo will undo the actions of the constituents in the reverse
+  order that they were added
+*/
+CompositeAction.prototype.Undo = function(){
+    console.log("Undo the actions!");
+    console.log("action list: " + this.action_list);
+    for(var i = this.action_list.length - 1; i >= 0; i--){
+        this.action_list[i].Undo();
+        console.log("action: " + this.action_list[i].toString());
+    }
+}
