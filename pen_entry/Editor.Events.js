6c0d357c38c3431251a58835436705c6aaff4b5f
cms5347 <cms5347@rit.edu>
2013-02-27 17:23:43 -0500

    Added a function for when the mouse is clicked in draw mode while typing.

--- pen_entry/Editor.Events.js
+++ pen_entry/Editor.Events.js
@@ -264,29 +264,7 @@ Editor.onMouseDown = function(e)
         break;
 
     case EditorState.MiddleOfText:
-        Editor.current_text.finishEntry();
-        if(Editor.current_action.toString() == "EditText")
-            Editor.current_action.set_current_text(Editor.current_text.text);
-        else if(Editor.current_action.toString() == "AddSegments")
-            Editor.current_action.buildSegmentXML();                
-
-        // Modification: reset to drawing.
-        // But only switch to draw mode if we click on the canvas.
-        var canvasDims = document.getElementById('equation_canvas').getBoundingClientRect();
-        var toolbarDims = document.getElementById('toolbar').getBoundingClientRect();
-
-        if (! (e.pageY > toolbarDims.bottom && e.pageY < canvasDims.bottom) &&
-            (e.pageX > 0 && e.pageX < canvasDims.right )) {
-            break; 
-        } else {
-            // build a new stroke object and save reference so we can add new points
-            Editor.current_stroke = new PenStroke(Editor.mouse_position.x,Editor.mouse_position.y, 6);
-            Editor.add_action(new AddSegments(new Array(Editor.current_stroke)));
-            Editor.add_segment(Editor.current_stroke);            
-            
-            Editor.state = EditorState.MiddleOfStroke;
-        }
-        RenderManager.render();
+        DrawMode.stopTextInput(e);
         break;
 
     case EditorState.ReadyForText:
