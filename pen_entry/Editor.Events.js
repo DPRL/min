cce12b1135a68e27587209762eb4a096e3f5200a
cms5347 <cms5347@rit.edu>
2013-02-27 16:02:04 -0500

    Moved ReadyToRectangleSelect from Editor.Events to Editor.RectSelectMode.

--- pen_entry/Editor.Events.js
+++ pen_entry/Editor.Events.js
@@ -256,30 +256,7 @@ Editor.onMouseDown = function(e)
         StrokeSelectMode.onMouseDown(e);
         break;        
     case EditorState.ReadyToRectangleSelect:
-        // get the segments that are under the mouse click
-        var click_result = CollisionManager.get_point_collides_bb(Editor.mouse_position);
-
-        if(click_result.length > 0)
-        {
-            // nothing selected at the moment, add all below mouse click to selected
-            // add the last guy in the list (probably most recently added) to selected set
-            var segment = click_result.pop();
-            for(var k = 0; k < Editor.segments.length; k++)
-                if(Editor.segments[k].set_id == segment.set_id)
-                    Editor.add_selected_segment(Editor.segments[k]);
-            
-            Editor.add_action(new TransformSegments(Editor.selected_segments));
-            Editor.state = EditorState.SegmentsSelected;
-
-            setTimeout(function() { Editor.touchAndHold(e); }, Editor.touchAndHoldTimeout);
-        }
-        else
-        {
-            Editor.start_rect_selection = Editor.mouse_position.clone();
-            Editor.end_rect_selection  = Editor.mouse_position.clone();
-            Editor.state = EditorState.RectangleSelecting;
-        }
-        RenderManager.render();
+        RectSelectMode.onMouseDown(e);
         break;
 
     case EditorState.SegmentsSelected:
