fd158a460378374582b93e385760fa30a3a27e26
cms5347 <cms5347@rit.edu>
2013-03-01 14:22:56 -0500

    Added some documentation comments to EditorMode.

--- pen_entry/Editor.EditorMode.js
+++ pen_entry/Editor.EditorMode.js
@@ -5,8 +5,20 @@ It defines the interface that an editor mode should have.
 
 function EditorMode(){}
 
+/*
+This function is used for when we first switch into a mode in order
+to set things up. e.g bind the event listeners, change colors, etc.
+*/
 EditorMode.prototype.init_mode = function(){
     alert("init_mode not implemented!");
+}
+
+/*
+This function is used for when we are finished with a mode
+and need to do teardown, such as unbinding event handlers.
+*/
+EditorMode.prototype.close_mode = function(){
+    alert("close_mode not implemented!");
 }
 
 EditorMode.prototype.onMouseDown = function(e){
@@ -23,9 +35,5 @@ EditorMode.prototype.onMouseUp = function(e){
 
 EditorMode.prototype.onDoubleClick = function(e){
     alert("onDoubleClick not implemented!");
-}
-
-EditorMode.prototype.close_mode = function(){
-    alert("close_mode not implemented!");
 }
 
