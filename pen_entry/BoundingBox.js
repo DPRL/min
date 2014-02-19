e2d0f4f4dd3324079dfb925a0b9c0d17c45bb985
Christopher Sasarak <cms5347@rit.edu>
2014-02-19 13:22:16 -0500

    Added GPL header to source files

--- pen_entry/BoundingBox.js
+++ pen_entry/BoundingBox.js
@@ -1,3 +1,21 @@
+This file is part of Min.
+
+Min is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Min is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Min.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright 2014 Richard Pospesel, Kevin Hart, Lei Hu, Siyu Zhu, David Stalnaker,
+Christopher Sasarak, Robert LiVolsi, Awelemdy Orakwue, and Richard Zanibbi
+(Document and Pattern Recognition Lab, RIT) 
 /*
 	This file defines an object which represents the bounding box which surrounds objects
     on the canvas when they are clicked or selected.
